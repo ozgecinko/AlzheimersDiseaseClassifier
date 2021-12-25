49ea5f38dd37aae4a4a04790d895cf4797a42285
Kader Miyanyedi <kadermiyanyedi@hotmail.com>
2021-12-25 14:02:43 +0300

    update app file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.metrics import AUC
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,26 +10,33 @@ dependencies = {
     'auc_roc': AUC
 }
 
-model = load_model('model.h5', compile=False)
+verbose_name = {
+	0: "test",
+	1: "test",
+	2: "test",
+	3: "test"
+}
+
+# Select model
+model = load_model('cnn_model.h5', compile=False)
 
 model.make_predict_function()
 
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(100,100))
-	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 100,100,3)
-	predict_x=model.predict(i) 
+	test_image = image.load_img(img_path, target_size=(176,176))
+	test_image = image.img_to_array(test_image)/255.0
+	test_image = test_image.reshape(1, 176,176,3)
+
+	predict_x=model.predict(test_image) 
 	classes_x=np.argmax(predict_x,axis=1)
-	#return dic[classes_x[0]]
+	
+	return verbose_name[classes_x[0]]
 
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
 	return render_template("index.html")
 
-@app.route("/about")
-def about_page():
-	return "Please subscribe  Artificial Intelligence Hub..!!!"
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
@@ -38,9 +46,9 @@ def get_output():
 		img_path = "static/" + img.filename	
 		img.save(img_path)
 
-		p = predict_label(img_path)
+		predict_result = predict_label(img_path)
 
-	return render_template("index.html", prediction = "", img_path = img_path)
+	return render_template("index.html", prediction = predict_result, img_path = img_path)
 
 
 if __name__ =='__main__':
