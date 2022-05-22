3f12b8f0122b02fd019d0765250ffe7702adc4e5
Kader Miyanyedi <kadermiyanyedi@hotmail.com>
2022-05-22 23:16:56 +0300

    add firebase package and config

---
+++ config.py
@@ -0,0 +1,9 @@
+firebase_config = {
+    "apiKey": "",
+    "authDomain": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": "",
+    "databaseURL": "",
+}
